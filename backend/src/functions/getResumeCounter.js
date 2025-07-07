6a861ad6b1802ee309e7a56cdfb9dee20629af4c
sanjeedastrek <sanjeeda.noor321@gmail.com>
2025-07-06 19:11:21 -0500

    azure function tools installed

--- backend/src/functions/getResumeCounter.js
+++ backend/src/functions/getResumeCounter.js
@@ -1,6 +1,6 @@
 const { app, input, output } = require('@azure/functions');
 
-// Define CosmosDB input binding (reads from DB)
+// Define Cosmos DB input binding (reads from DB)
 const cosmosInput = input.cosmosDB({
     databaseName: 'db-cloud-resume',         // Your DB name
     containerName: 'counter',                // Your container name

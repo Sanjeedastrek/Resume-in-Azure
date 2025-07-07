1c22ef60768fe7b4199d7c78893a8208662a54ad
sanjeedastrek <sanjeeda.noor321@gmail.com>
2025-07-06 20:59:25 -0500

    runtime added to func

--- backend/src/functions/getResumeCounter.js
+++ backend/src/functions/getResumeCounter.js
@@ -1,6 +1,6 @@
 const { app, input, output } = require('@azure/functions');
 
-// Define Cosmos DB input binding (reads from DB)
+// Define CosmosDB input binding (reads from DB)
 const cosmosInput = input.cosmosDB({
     databaseName: 'db-cloud-resume',         // Your DB name
     containerName: 'counter',                // Your container name

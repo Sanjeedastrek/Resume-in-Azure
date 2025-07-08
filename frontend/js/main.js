91f03505af36ce8925f2036e486b56c68916f940
sanjeedastrek <sanjeeda.noor321@gmail.com>
2025-07-08 00:35:15 -0500

    fixing main js 2

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -3,7 +3,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
 });
 
 const functionApi = 'http://localhost:7071/api/getResumeCounter';
-const functionApiAzure = "https://counter2.azurewebsites.net/api/getResumeCounter"
+const functionApiAzure = "https://counter2.azurewebsites.net/api/getResumeCounter?"
 
 const getVisitCount = () => {
     
